8520bf36b96537b7de44698148dc516a62afe9dc
dimpram101 <dimas2004211@gmail.com>
2023-11-22 22:46:00 +0800

    first commit and setup

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1D2227",
+        secondary: "#096FFD",
+        background: "#171C21"
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
+
