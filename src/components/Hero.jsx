b2dbb5fda5ef0bdefb9bc61e6d04a6332d4bc8fc
Cibobhap <11211025@student.itk.ac.id>
2023-11-24 21:20:46 +0800

    fitur Responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,27 @@
-import hero from '../../public/hero.png'
+import hero from '../../public/hero.png';
+import React from 'react';
+
 const Hero = () => {
     return (
-        <div className = "flex justify-center items-center flex-col bg-contain w-full min-h-screen" >
-            <img src={hero} alt="" className="w-full"/>
-            <div className="w-full  flex absolute z-10 h-full flex-row justify-center bg-black/50">
-                <div className = "w-[35%] flex-col  flex justify-center items-center  gap-5">
-                    <h1 className ="text-[3.9375rem] text-center font-bold" >
-                    Your <span className="text-[#096FFD] bg-[#FFFFFF]">Best Movies Data </span>
-                    <span className="break-words">to Help You Find Your Mood!</span> 
+        <div className="relative w-full">
+            <img src={hero} alt="" className="w-full h-auto" />
+            <div className="absolute inset-0 flex items-center justify-center bg-black/50">
+                <div className="w-full md:w-[35%] flex-col flex justify-center items-center text-white p-4">
+                    <h1 className="text-2xl md:text-4xl text-center font-bold mb-4">
+                        Your <span className="text-[#096FFD] bg-[#FFFFFF]">Best Movies Data </span>
+                        <span className="break-words">to Help You Find Your Mood!</span>
                     </h1>
-                    <h2 className = "text-[2.25rem] text-center font-thin">
-                    Boost your mood in every moment find 
-                    the treasure to pleasure your chill time.
+                    <h2 className="text-lg md:text-2xl text-center font-thin mb-4">
+                        Boost your mood in every moment, find
+                        the treasure to pleasure your chill time.
                     </h2>
-                    <div className = "bg-[#096FFD]" >
-                        <p className = "px-[7.44rem] py-[1.5rem] text-lg"  >Explore Now</p>
+                    <div className="bg-[#096FFD]">
+                        <p className="px-4 py-2 text-md md:text-lg">Explore Now</p>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
+};
 
-}
- export default Hero
+export default Hero;
