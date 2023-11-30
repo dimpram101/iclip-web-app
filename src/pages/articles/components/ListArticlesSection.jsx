d40333d114d18057b7e5c0a07943b6bbd8e570f4
jihanapriliani <11211046@student.itk.ac.id>
2023-11-30 14:54:19 +0800

    fixing list article spacing

--- src/pages/articles/components/ListArticlesSection.jsx
+++ src/pages/articles/components/ListArticlesSection.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function ListArticlesSection() {
   return (
-    <section className="max-w-screen-2xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+    <section className="mb-16 max-w-screen-2xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 ">
     {Array.from({ length: 8 }).map((_, index) => (
-      <div key={index} className="flex-1 shadow-lg shadow-black-500/50 hover:cursor-pointer">
+      <div key={index} className="p-4 flex-1 shadow-lg shadow-black-500/50 hover:cursor-pointer">
         <img
           src="https://images.unsplash.com/photo-1682687982029-edb9aecf5f89?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           className="flex-1 h-[200px] w-[100%]"
