910c6990f867b93b0f31c4524099e4106c0134fc
jihanapriliani <11211046@student.itk.ac.id>
2023-12-02 16:30:26 +0800

    fixing headline section responsiveness in mobile

--- src/pages/articles/components/HeadlineSection.jsx
+++ src/pages/articles/components/HeadlineSection.jsx
@@ -8,6 +8,17 @@ export default function HeadlineSection({ firstHeadline = {}, secondHeadline = {
     navigation(`/articles/${title}`)
   }
 
+
+  function limitWords(description, limit) {
+    const words = description.split(' ');
+  
+    if (words.length > limit) {
+      return words.slice(0, limit).join(' ') + '...';
+    } else {
+      return description;
+    }
+  }
+
   const LoadingSkeleton = () => (
     <>
         <div className='flex-4 flex w-[70%] p-4 mr-8 flex shadow-lg shadow-black-500/50 border border-primary shadow'>
@@ -54,32 +65,42 @@ export default function HeadlineSection({ firstHeadline = {}, secondHeadline = {
   )
 
   return (
-    <section className='flex flex-col md:flex-row h-[70vh] max-w-screen-2xl mx-auto py-12'>
+    <section className='flex flex-col md:flex-row lg:h-[70vh] max-w-screen-2xl mx-auto py-12 gap-4'>
 
       {isLoading ? (
         <LoadingSkeleton />
       ): (
         <>
-          <div className='flex-4   p-4 mr-8 flex shadow-lg shadow-black-500/50' onClick={() => handleArticleClicked(firstHeadline.title)}>
+          <div className='lg:flex-4 py-8 px-2 md:mr-8 flex shadow-lg shadow-black-500/50 flex flex-col md:flex-row' onClick={() => handleArticleClicked(firstHeadline.title)}>
             <div className='flex flex-col mr-3 max-w-lg' >
               <h1 className='text-4xl mb-2 underline underline-offset-4 hover:cursor-pointer'>
                 {firstHeadline.title || ""}
               </h1>
-              <p className='flex-1 text-lg text-slate-400'>
-                {firstHeadline.description || ""}
+              <p className='flex-1 text-lg text-slate-400 hidden md:block'>
+                {limitWords(firstHeadline.description, 80) || ""}
               </p>
+
+              <p className='flex-1 text-md text-slate-400 md:hidden'>
+                {limitWords(firstHeadline.description, 20) || ""}
+              </p>
+
             </div>
-            <img src={firstHeadline.image_url || ""} className='flex-1 h-[400px] object-cover max-w-xl' alt="" />
+            <img src={firstHeadline.image_url || ""} className='flex-1 lg:h-[400px] object-cover max-w-xl' alt="" />
           </div>
 
-          <div className='flex-1 w-[30%] shadow-lg shadow-black-500/50' onClick={() => handleArticleClicked(secondHeadline.title)}>
-            <img src={secondHeadline.image_url || ""} className='flex-1 h-[200px] w-[100%] object-cover' alt="" />
+          <div className='lg:flex-1 lg:w-[30%]  py-8 px-2  shadow-lg shadow-black-500/50' onClick={() => handleArticleClicked(secondHeadline.title)}>
+            <img src={secondHeadline.image_url || ""} className='flex-1 lg:h-[200px] lg:w-[100%] object-cover' alt="" />
             <h1 className='text-2xl my-2 hover:underline hover:cursor-pointer'>
               {secondHeadline.title || ""}
             </h1>
-            <p className='text-slate-400 text-lg'>
-              {secondHeadline.description || ""}
+            <p className='text-slate-400 text-lg hidden md:block'>
+              {limitWords(secondHeadline.description, 30) || ""}
             </p>
+
+            <p className='flex-1 text-md text-slate-400 md:hidden'>
+                {limitWords(firstHeadline.description, 15) || ""}
+              </p>
+
           </div>
         </>
       )}
